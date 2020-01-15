920928c695ed052244a9a23e20a215342bd833c8
Alexsuperfly <alexcsumner@gmail.com>
2020-01-15 12:13:17 -0500

    lint and refactor GWD boss loot tables

--- src/monsters/GeneralGraardor.ts
+++ src/monsters/GeneralGraardor.ts
@@ -7,37 +7,41 @@ const GeneralGraardorArmorTable = new LootTable()
 	.add('Bandos tassets')
 	.add('Bandos boots');
 
+const UniqueTable = new LootTable()
+	.add(GeneralGraardorArmorTable, undefined, 4)
+	.add(ShardTable, undefined, 2)
+	.addItem('Bandos hilt')
+	.addItem('Coins', [20_100, 20_600], 5);
+
 export const GeneralGraardorTable = new LootTable()
 	.every('Big bones')
-	.add(GeneralGraardorArmorTable, undefined, 4)
+	.add(UniqueTable, undefined, 3)
 	.tertiary(250, 'Clue scroll (elite)')
-	.add(ShardTable, undefined, 2)
 	.tertiary(400, 'Long bone')
-	.addItem('Bandos hilt', 1, 1)
 	.tertiary(5000, 'Pet general graardor')
 	.tertiary(5013, 'Curved bone')
 
 	/* Weapons and armour */
-	.addItem('Rune longsword', 1, 32)
-	.addItem('Rune 2h sword', 1, 32)
-	.addItem('Rune platebody', 1, 32)
-	.addItem('Rune pickaxe', 1, 24)
-
+	.addItem('Rune longsword', 1, 8)
+	.addItem('Rune 2h sword', 1, 8)
+	.addItem('Rune platebody', 1, 8)
+	.addItem('Rune pickaxe', 1, 6)
 
 	/* Other */
-	.addItem('Coins', [19500, 20000], 112)
-	.addItem('Grimy snapdragon', 3, 32)
-	.addItem('Snapdragon seed', 1, 32)
-	.addItem('Super restore(4)', 3, 32)
-	.addItem('Adamantite ore', [15, 20], 32)
-	.addItem('Coal', [115, 120], 32)
-	.addItem('Magic logs', [15, 20], 32)
-	.addItem('Nature rune', [65, 70], 32)
-	.addItem('Coins', [20100, 20600], 5)
+	.addItem('Coins', [19500, 20000], 28)
+	.addItem('Grimy snapdragon', 3, 8)
+	.addItem('Snapdragon seed', 1, 8)
+	.addItem('Super restore(4)', 3, 8)
+	.addItem('Adamantite ore', [15, 20], 8)
+	.addItem('Coal', [115, 120], 8)
+	.addItem('Magic logs', [15, 20], 8)
+	.addItem('Nature rune', [65, 70], 8)
 
+	.add(GWRareDropTable, undefined, 8)
+	.add(GWGemTable, undefined, 2);
 
-
-	.add(GWRareDropTable, undefined, 32)
-	.add(GWGemTable, undefined, 8);
-
-export default new SimpleMonster({ id: 2215, name: 'General Graardor', table: GeneralGraardorTable });
+export default new SimpleMonster({
+	id: 2215,
+	name: 'General Graardor',
+	table: GeneralGraardorTable
+});
--- src/monsters/KrilTsutsaroth.ts
+++ src/monsters/KrilTsutsaroth.ts
@@ -2,50 +2,52 @@ import LootTable from '../structures/LootTable';
 import GWRareDropTable, { GWGemTable, ShardTable } from './GWRareDropTable';
 import SimpleMonster from '../structures/SimpleMonster';
 
-const AttStrTable = new LootTable()
-    .addItem([
-            ['Super attack(3)', 3],
-            ['Super strength(3)', 3]
-    ]);
+const AttStrTable = new LootTable().addItem([
+	['Super attack(3)', 3],
+	['Super strength(3)', 3]
+]);
 
-const RestoreBrewTable = new LootTable()
-    .addItem([
-            ['Super restore(3)', 3],
-            ['Zamorak brew(3)', 3]
-    ]);
+const RestoreBrewTable = new LootTable().addItem([
+	['Super restore(3)', 3],
+	['Zamorak brew(3)', 3]
+]);
+
+const UniqueTable = new LootTable()
+	.addItem('Steam battlestaff', 1, 4)
+	.addItem('Zamorakian spear', 1, 4)
+	.addItem('Staff of the dead')
+	.addItem('Zamorak hilt')
+	.add(ShardTable, undefined, 2);
 
 export const KrilTsutsarothTable = new LootTable()
 	.every('Ashes')
-	.addItem('Steam battlestaff', 1, 4)
-	.addItem('Zamorakian spear', 1, 4)
-	.addItem('Staff of the dead', 1, 1)
-	.addItem('Zamorak hilt', 1, 1)
-	.add(ShardTable, undefined, 2)
+	.add(UniqueTable, undefined, 3)
 	.tertiary(250, 'Clue scroll (elite)')
 	.tertiary(5000, `Pet k'ril tsutsaroth`)
 
 	/* Weapons and armour */
-	.addItem('Adamant arrow(p++)', [295, 300], 32)
-	.addItem('Rune scimitar', 1, 32)
-	.addItem('Adamant platebody', 1, 32)
-	.addItem('Rune platelegs', 1, 28)
-	.addItem('Dragon dagger(p++)', 1, 8)
+	.addItem('Adamant arrow(p++)', [295, 300], 8)
+	.addItem('Rune scimitar', 1, 8)
+	.addItem('Adamant platebody', 1, 8)
+	.addItem('Rune platelegs', 1, 7)
+	.addItem('Dragon dagger(p++)', 1, 2)
 
 	/* Potions */
-	.add(AttStrTable, undefined, 32)
-	.add(RestoreBrewTable, undefined, 32)
-
+	.add(AttStrTable, undefined, 8)
+	.add(RestoreBrewTable, undefined, 8)
 
 	/* Other */
-	.addItem('Coins', [19500, 20000], 132)
-	.addItem('Grimy lantadyme', 10, 32)
-	.addItem('Lantadyme seed', 3, 32)
-	.addItem('Death rune', [120, 125], 32)
-	.addItem('Blood rune', [80, 85], 32)
+	.addItem('Coins', [19500, 20000], 33)
+	.addItem('Grimy lantadyme', 10, 8)
+	.addItem('Lantadyme seed', 3, 8)
+	.addItem('Death rune', [120, 125], 8)
+	.addItem('Blood rune', [80, 85], 8)
 
+	.add(GWRareDropTable, undefined, 8)
+	.add(GWGemTable, undefined, 2);
 
-
-	.add(GWRareDropTable, undefined, 32)
-	.add(GWGemTable, undefined, 8);
-
-export default new SimpleMonster({ id: 3129, name: `K'ril Tsutsaroth`, table: KrilTsutsarothTable });
+export default new SimpleMonster({
+	id: 3129,
+	name: `K'ril Tsutsaroth`,
+	table: KrilTsutsarothTable
+});
